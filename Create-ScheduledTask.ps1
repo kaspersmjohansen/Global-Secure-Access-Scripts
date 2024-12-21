cd6b6fddefe04d9b7cd548ef1bf89d2cf7625eac
Kasper Johansen <kaspersmjohansen@gmail.com>
2024-12-21 15:34:43 +0100

    Initial commit

---
+++ Create-ScheduledTask.ps1
@@ -0,0 +1,29 @@
+
+# Define CIM object variables
+# This is needed for accessing the non-default trigger settings when creating a schedule task using Powershell
+$Class = cimclass MSFT_TaskEventTrigger root/Microsoft/Windows/TaskScheduler
+$Trigger = $class | New-CimInstance -ClientOnly
+$Trigger.Enabled = $true
+$Trigger.Subscription = "<QueryList><Query Id=`"0`" Path=`"Microsoft-Windows-NetworkProfile/Operational`"><Select Path=`"Microsoft-Windows-NetworkProfile/Operational`">*[System[Provider[@Name='Microsoft-Windows-NetworkProfile'] and EventID=4004]]</Select></Query></QueryList>"
+
+# Find Builtin\Users name on localized Windows
+# Well known SIDs are found here - https://learn.microsoft.com/en-us/windows-server/identity/ad-ds/manage/understand-security-identifiers#well-known-sids
+$GroupName = Get-LocalGroup -SID "S-1-5-32-545"
+
+# Define additional variables containing scheduled task action and scheduled task principal
+$A = New-ScheduledTaskAction -Execute powershell.exe -Argument "-executionpolicy bypass -WindowStyle Hidden -NonInteraktive -NoLogo -file C:\ProgramData\GSAscripts\Detect-CorpNetwork.ps1"
+$P = New-ScheduledTaskPrincipal -GroupId $GroupName -RunLevel Limited
+$S = New-ScheduledTaskSettingsSet -Compatibility Win8 -DontStopIfGoingOnBatteries -AllowStartIfOnBatteries -DontStopOnIdleEnd
+
+# Cook it all up and create the scheduled task
+$RegSchTaskParameters = @{
+    TaskName    = "Global Secure Access - Detect network state"
+    Description = "Powershell script executed on event id 4004, to detect if the device is on the corporate network. GSA Private Access is disabled if the device is on the corporate network"
+    TaskPath    = "\"
+    Action      = $A
+    Principal   = $P
+    Settings    = $S
+    Trigger     = $Trigger
+}
+
+Register-ScheduledTask @RegSchTaskParameters
--- Detect-CorpNetwork.ps1
+++ Detect-CorpNetwork.ps1
@@ -1,26 +1,64 @@
 $DNSSuffix = "johansen.local"
 $PrivateConnectorFQDN = "srvapc01.johansen.local"
 $PrivateConnectorIP = "192.168.30.245"
-$HostCheck = "FQDN" #IP or #FQDN
+$HostCheck = "DNS" #IP or #FQDN or #DNS
 
-# Get active and connected Network Interface Card - Exclude Hyper-V virtual ethernet switches and Hyper-V network adapters
-$ActivePhysicalNIC = Get-NetAdapter | Where-Object {$_.Status -ne "Disconnected" -and $_.InterfaceDescription -notlike "Hyper-V Virtual Ethernet*"}
-
-# Get active network interface card IP configuration
-$IPconfiguration = Get-NetIPConfiguration -InterfaceIndex $ActivePhysicalNIC.ifIndex
-
-# Detect if device is connected to local corporate network and disable GSA Private Access
-If ($($IPconfiguration.NetProfile.Name) -eq "$DNSSuffix")
+# Detect if device is connected to local corporate network, if not disable GSA Private Access
+If ($HostCheck -eq "DNS")
 {
-    Write-Host "On corp network - DNS Suffix: $($IPconfiguration.NetProfile.Name)"
-    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Global Secure Access Client"  -Name "IsPrivateAccessDisabledByUser" -Value 1 -Type DWord -Force
-}
-elseif (Resolve-DnsName -Name $PrivateConnectorFQDN -Type A -ErrorAction SilentlyContinue)
-{
-    Write-Host "On corp network - DNS Suffix: $($IPconfiguration.NetProfile.Name)"
-    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Global Secure Access Client"  -Name "IsPrivateAccessDisabledByUser" -Value 1 -Type DWord -Force
-}
+    Write-Host "Determine corp network access based on DNS suffix" -ForegroundColor Cyan
+    # Get active and connected Network Interface Card - Exclude Hyper-V virtual ethernet switches and Hyper-V network adapters
+    $ActivePhysicalNIC = Get-NetAdapter | Where-Object {$_.Status -ne "Disconnected" -and $_.InterfaceDescription -notlike "Hyper-V Virtual Ethernet*"}
 
-else {
-    <# Action when all if and elseif conditions are false #>
+    # Get active network interface card IP configuration
+    $IPconfiguration = Get-NetIPConfiguration -InterfaceIndex $ActivePhysicalNIC.ifIndex
+
+    If ($($IPconfiguration.NetProfile.Name) -eq "$DNSSuffix")
+    {
+        Write-Host "Current DNS Suffix: $($IPconfiguration.NetProfile.Name)" -ForegroundColor Cyan
+        Write-Host "Connected to corp network" -ForegroundColor Cyan
+        Write-Host "Global Secure Access client is: DISABLED" -ForegroundColor Cyan
+        Set-ItemProperty -Path "HKCU:\Software\Microsoft\Global Secure Access Client"  -Name "IsPrivateAccessDisabledByUser" -Value 1 -Type DWord -Force
+    }
+        else 
+        {
+            Write-Host "On unknown network" -ForegroundColor Cyan
+            Write-Host "Global Secure Access client is: ENABLED" -ForegroundColor Cyan
+            Set-ItemProperty -Path "HKCU:\Software\Microsoft\Global Secure Access Client"  -Name "IsPrivateAccessDisabledByUser" -Value 0 -Type DWord -Force
+        }
 }
+        If ($HostCheck -eq "FQDN")
+        {
+            Write-Host "Determine corp network access based on resolving FQDN" -ForegroundColor Cyan
+            If (Resolve-DnsName -Name $PrivateConnectorFQDN -Type A -ErrorAction SilentlyContinue)
+            {
+                Write-Host "Resolved FQDN: $PrivateConnectorFQDN" -ForegroundColor Cyan
+                Write-Host "COnnected to corp network" -ForegroundColor Cyan
+                Write-Host "Global Secure Access client is: DISABLED" -ForegroundColor Cyan
+                Set-ItemProperty -Path "HKCU:\Software\Microsoft\Global Secure Access Client"  -Name "IsPrivateAccessDisabledByUser" -Value 1 -Type DWord -Force
+            }
+                else 
+                {
+                    Write-Host "On unknown network" -ForegroundColor Cyan
+                    Write-Host "Global Secure Access client is: ENABLED" -ForegroundColor Cyan
+                    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Global Secure Access Client"  -Name "IsPrivateAccessDisabledByUser" -Value 0 -Type DWord -Force
+                }
+        }               
+                If ($HostCheck -eq "IP")
+                {
+                    Write-Host "Determine corp network access based on IP ping" -ForegroundColor Cyan
+                    If (Test-Connection $PrivateConnectorIP -Count 3 -Quiet -ErrorAction SilentlyContinue)
+                    {
+                        Write-Host "Pinged IP: $PrivateConnectorIP"-ForegroundColor Cyan
+                        Write-Host "On corp network" -ForegroundColor Cyan
+                        Write-Host "Global Secure Access client is: DISABLED" -ForegroundColor Cyan
+                        Set-ItemProperty -Path "HKCU:\Software\Microsoft\Global Secure Access Client"  -Name "IsPrivateAccessDisabledByUser" -Value 1 -Type DWord -Force    
+                    }
+                        else
+                        {
+                            Write-Host "On unknown network" -ForegroundColor Cyan
+                            Write-Host "Global Secure Access client is: ENABLED" -ForegroundColor Cyan
+                            Set-ItemProperty -Path "HKCU:\Software\Microsoft\Global Secure Access Client"  -Name "IsPrivateAccessDisabledByUser" -Value 0 -Type DWord -Force
+                        }
+                }
+                    
